cc7dffeec4f2b548aaf3bcf6c4eff04e9a00cd9d
ezzeldeen377 <ezzeldeentv20@gmail.com>
2025-01-28 15:13:51 +0200

    init branch

---
+++ lib/features/disease_history/presentation/pages/disease_history_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class Disease_historyPage extends StatefulWidget {
+	@override
+	createState() => _Disease_historyPage();
+}
+class _Disease_historyPage extends State<Disease_historyPage> {
+	@override
+	Widget build(BuildContext context) {
+		return Scaffold(
+			appBar: AppBar(),
+		);
+	}
+}
